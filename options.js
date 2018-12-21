338a9d74a784fe8261f446fbc654427de7024fa2
agrawalrinkal <rinkalv063@gmail.com>
2018-12-20 18:27:19 -0600

    added css file and updated popup to show multiple colors, and change background and font color

--- options.js
+++ options.js
@@ -4,18 +4,40 @@
 
 'use strict';
 
+// Get the page instance
 let page = document.getElementById('buttonDiv');
-  const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-  function constructOptions(kButtonColors) {
-    for (let item of kButtonColors) {
-      let button = document.createElement('button');
-      button.style.backgroundColor = item;
-      button.addEventListener('click', function() {
-        chrome.storage.sync.set({color: item}, function() {
-          console.log('color is ' + item);
-        })
-      });
-      page.appendChild(button);
-    }
+
+// Create a map for <backgroundcolor, fontcolor>
+const colorMap = new Map();
+colorMap.set('#3aa757', '#172617');
+colorMap.set('#e8453c', '#000000');
+colorMap.set('#f9bb2d', '#1D1905');
+colorMap.set('#4688f1', '#E4F1FE');
+colorMap.set('#f7f7f7', '#2a2c33');
+colorMap.set('#2a2c33', '#f7f7f7');
+
+function constructOptions(colorMap) {
+  for (let item of colorMap.keys()) {
+    let button = document.createElement('button');
+    button.style.backgroundColor = item;
+    button.addEventListener('click', function() {
+      // Update the extension global store value and print on console
+      chrome.storage.sync.set({color: item}, function() {
+        console.log('color is ' + item);
+      })
+
+      //Update the page content using the user selected value
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          chrome.tabs.executeScript(
+             tabs[0].id,
+             {
+              code: 'document.body.style.backgroundColor = "' + item + '";' +
+                    'document.body.style.color = "' + colorMap.get(item) + '";'
+             }
+          );
+       });
+    });
+    page.appendChild(button);
   }
-  constructOptions(kButtonColors);
+}
+constructOptions(colorMap);
